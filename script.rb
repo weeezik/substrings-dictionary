81f77d097762aea327a54b97975a061f255b0b7c
weeezik <mkihanya.33@gmail.com>
2024-03-24 20:41:27 +0000

    Recent changes
    About to make larger change with .include? method

--- script.rb
+++ script.rb
@@ -5,7 +5,8 @@ def substrings (string, dictionary)
   new_string = string.downcase.gsub(/\W/, " ") #delete all punctuation
   string_array = new_string.split(" ")  #make lowercase
 
-  overlap_array = dictionary - (dictionary - string_array)
+  overlap_array = dictionary - (dictionary - string_array) #calculate overlapping strings
+
   overlap_array.reduce(Hash.new(0)) do |substring_count_hash, substring|
     substring_count_hash[substring] = 1
     substring_count_hash
