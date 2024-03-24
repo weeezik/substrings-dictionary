390ea76bb42a3905db7bdba0226200d90b6dba0a
weeezik <mkihanya.33@gmail.com>
2024-03-24 20:54:44 +0000

    Nested each method approach
    To check similarity to dictionary for each substring.

--- script.rb
+++ script.rb
@@ -1,15 +1,25 @@
+require 'pry-byebug'
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 
 def substrings (string, dictionary)
   new_string = string.downcase.gsub(/\W/, " ") #delete all punctuation
   string_array = new_string.split(" ")  #make lowercase
+  
+  binding.pry
+  # for each substring from given string check to see if it 
+  # matches any of the words in the dictionary
+  string_array.each do |substring|
+    dictionary.each do |dictstring|
+    if dictstring == substring
+      substring
+    else
+      next
+    end
+    binding.pry
+  end
 
-  overlap_array = dictionary - (dictionary - string_array) #calculate overlapping strings
-
-  overlap_array.reduce(Hash.new(0)) do |substring_count_hash, substring|
-    substring_count_hash[substring] = 1
-    substring_count_hash
   end
 
 end
